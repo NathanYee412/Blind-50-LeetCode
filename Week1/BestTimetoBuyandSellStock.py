1acfbbf6f20a5f698e5040c85aaad8cf3dd6c5e8
NathanYee412 <nyee@csu.fullerton.edu>
2022-01-26 22:49:13 -0800

    stock question updated

--- Week1/BestTimetoBuyandSellStock.py
+++ Week1/BestTimetoBuyandSellStock.py
@@ -1,22 +1,56 @@
+# My solution using two while loops
+
 class Solution:
     def maxProfit(self, prices) -> int:
         # left and right pointer
-        l, r = 0, len(prices) - 1
+        l, r = 0, 1
         
-        lowDay = 10000
-        highDay = 0
+        maxDayIndex = 0
+        maxDayPrice = 0
+        minDayIndex = 0
+        minDayPrice = 10000
         
-        while l < r:
-            if prices[l] < lowDay:
-                lowDay = prices[l]
-                
-            if prices[r] > highDay:
-                highDay = prices[r]
-                
-            l += 1
-            r -= 1
+        while r != (len(prices) - 1):
+            #find max profit past l
+            if prices[r] > maxDayPrice:
+                maxDayPrice = prices[r]
+                maxDayIndex = r
             
-        if highDay - lowDay < 0:
+            r += 1
+        
+        while l < maxDayIndex:
+            #find min day before max day
+            if prices[l] < minDayPrice:
+                minDayPrice = prices[l]
+                minDayIndex = l
+            
+            l += 1
+        
+        if prices[maxDayIndex] - prices[minDayIndex] <= 0:
             return 0
         else:
-            return highDay - lowDay
+            return prices[maxDayIndex] - prices[minDayIndex]
+        
+
+# Correct solution
+# Moving window using two pointers
+
+class Solution:
+    def maxProfit(self, prices) -> int:
+        # left and right pointer
+        l, r = 0, 1
+        
+        maxP = 0
+        
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                if profit > maxP:
+                    maxP = profit
+            else:
+                l = r
+            
+            r += 1
+            
+        return maxP
+            
